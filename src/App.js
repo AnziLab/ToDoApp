1c91c47171a55dd79bf6f1fb0d89782ad05a2365
Jihoon from School <gorgeouskid@naver.com>
2021-04-21 15:16:26 +0900

    11강 완료...라고 하기엔 너무 복잡해서 중간부분은 안 봤음. 다음 임머로 넘어갈거임

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-
+function createBulkTodos(){
+  const array = [];
+  for (let i=0; i<2500; i++){
+    array.push({
+      id:i+1,
+      text: `Thing to do ${i+1}`,
+      cheched: false,
+    });
+  }
+  return array;
+}
 const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: 'learn basics of react',
-      checked: true
-    },
-    {
-      id: 2,
-      text: 'wanna go home',
-      checked: false
-    },
-    {
-      id: 3,
-      text: 'this is react',
-      checked: true
-    },
-  ]);
+  const [todos, setTodos] = useState(createBulkTodos);
 
 const nextId = useRef(4);
 
@@ -32,27 +26,26 @@ const onInsert = useCallback(
       text,
       checked: false,
     };
-    setTodos(todos.concat(todo));
+    setTodos(todos => todos.concat(todo));
     nextId.current +=1;
   },
-  [todos],
-);
+  []);
 
 const onRemove = useCallback(
   id=>{
-    setTodos(todos.filter(todo => todo.id!==id));
+    setTodos(todos => todos.filter(todo => todo.id!==id));
   },
-  [todos],
+  []
 );
 const onToggle = useCallback(
   id=>{
-    setTodos(
+    setTodos(todos =>
       todos.map(todo =>
         todo.id === id ? {...todo, checked: !todo.checked}: todo,
         ),
     );
   },
-  [todos],
+  [],
 );
   return (
     <TodoTemplate>
